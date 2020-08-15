c9c5ebc7447dd6f5352fd8d1af1c0e5310e4527d
YuvalsTwingo <sedler@twingo.co.il>
2020-08-15 18:55:52 +0300

    added socials api

--- server/controllers/userCourse.js
+++ server/controllers/userCourse.js
@@ -41,7 +41,13 @@ module.exports = {
         const rows = await getUserCoursesDetailedByUserId(userId);
         res.setHeader("content-type", "application/json");
         res.send(JSON.stringify(rows));
-    }
+    },
+
+    async handleGetTopPopular(req, res) {
+        const rows = await getTopPopular();
+        res.setHeader("content-type", "application/json");
+        res.send(JSON.stringify(rows));
+    },
 };
 
 /* DB ACCESSORS */
@@ -76,11 +82,22 @@ async function getUserCoursesByUserId(userId) {
     }
 }
 
+async function getTopPopular() {
+    try {
+        const query = 'with top_3 as (select course_number, count(*) cnt from user_courses where course_number in (select course_number from courses where course_type in (\'math\', \'elective\')) group by 1 order by 2 desc limit 3) select c.course_name from courses c join top_3 t on c.course_number = t.course_number order by cnt desc';
+        const results = await dbClient.query(query);
+        return results.rows;
+    } catch (e) {
+        console.log(e);
+        return [];
+    }
+}
+
 async function getUserCoursesDetailedByUserId(userId) {
     try {
         const results = await dbClient.query('select * from user_courses join courses on (user_courses.course_number = courses.course_number) where user_id = $1', [userId]);
         return results.rows;
-    } catch(e) {
+    } catch (e) {
         return [];
     }
 }
