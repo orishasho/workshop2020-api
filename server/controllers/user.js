794552a8b391e8d7a9ebe90334eb2a8c019a1af2
YuvalsTwingo <sedler@twingo.co.il>
2020-08-30 20:51:00 +0300

    added stuff

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,9 +5,7 @@ module.exports = {
         let result = {}
         try {
             const reqJson = req.body;
-            console.log(reqJson);
-            console.log(reqJson.user_email + "  " + reqJson.user_password);
-            await insertToUsers(reqJson.user_email, reqJson.user_password)
+            await insertToUsers(reqJson.user_email, reqJson.user_password, reqJson.college)
                 //console.log("in post");
                 //await insertToUsersCourses(reqJson);
             result.success = true;
@@ -22,6 +20,13 @@ module.exports = {
     async handleGetUserDetails(req, res) {
         const user_email = req.query["user_email"];
         const rows = await readUsersDetails(user_email);
+        res.setHeader("content-type", "application/json");
+        res.send(JSON.stringify(rows))
+    },
+
+    async handleGetUserCollegeByUserId(req, res) {
+        const user_id = req.query["user_id"];
+        const rows = await readUserCollege(user_id);
         res.setHeader("content-type", "application/json");
         res.send(JSON.stringify(rows))
     },
@@ -45,9 +50,9 @@ module.exports = {
 
 /* DB ACCESSORS */
 
-async function insertToUsers(user_email, user_password) {
+async function insertToUsers(user_email, user_password, college) {
     try {
-        await dbClient.query("insert into users (user_email, password) values ($1, $2)", [user_email, user_password]);
+        await dbClient.query("insert into users (user_email, password, college) values ($1, $2, $3)", [user_email, user_password, college]);
         return true
     } catch (e) {
         console.log(e);
@@ -58,6 +63,17 @@ async function insertToUsers(user_email, user_password) {
 async function readUsersDetails(user_email) {
     try {
         const results = await dbClient.query("select * from users where user_email = $1", [user_email]);
+        return results.rows;
+    } catch (e) {
+        console.log(e);
+        return [];
+    }
+}
+
+async function readUserCollege(user_id) {
+    try {
+        console.log(user_id);
+        const results = await dbClient.query("select college from users where user_id = $1", [user_id]);
         return results.rows;
     } catch (e) {
         console.log(e);
